52792e7ea1dbfbaa2a76f86f940c6043c455dba1
Yash-K-B <yashwantkumar027@gmail.com>
2020-01-20 21:20:23 +0530

    contact us

--- js/contactus.js
+++ js/contactus.js
@@ -1,3 +1,23 @@
+$('.heading').click(function() {
+    if ($(this).hasClass('selected')) return;
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+    $content_id = "#" + $(this).attr('id') + "-b";
+    //console.log($content_id);
+
+    //console.log($('.d-block').attr('id'));
+
+    //hide previous data
+    $('.d-block').addClass('d-none');
+    $('.d-block').removeClass('d-block');
+
+    //show current element
+    $($content_id).removeClass('d-none');
+    $($content_id).addClass('d-block');
+
+    //console.log($($content_id).attr('class'));
+});
+
 $(document).ready(function() {
     if ($(document).scrollTop() > (window.innerHeight * 0.15)) { // check if user scrolled more than 50 from top of the browser window
         $(".navbar").addClass("bg-info-1");
